0b814caab314a0af8cd6b9a612fe174331cc5bf3
Joonas Jauhiainen2 <joonas.jauhiainen@eficode.com>
2015-08-14 16:29:15 +0300

    Added placehoder for error messages.

--- assets/js/controllers/HomeController.js
+++ assets/js/controllers/HomeController.js
@@ -1,13 +1,17 @@
 app.controller('HomeController', ['$scope', 'firehydrants', function($scope, fireHydrantService){
 	$scope.map = {center: {latitude: 60.218184, longitude: 24.812150}, zoom: 15};
 	$scope.showFireHydrantContainer = false;
-
+	$scope.isServerError = false;
+	$scope.serverErrorMessage = "";
 	$scope.$watch("showFireHydrantContainer", function(){
 		if ($scope.showFireHydrantContainer === true){
 			fireHydrantService.getFireHydrants().success(function(data){
 				$scope.firehydrants = data;
 				console.log($scope.firehydrants);
 			})
+			.error(function(err){
+				$scope.isServerError = true;
+			});
 		}
 	});
 }]);
