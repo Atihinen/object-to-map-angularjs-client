701271d0626cf190f38b644a643f8a02f159fb73
Joonas Jauhiainen2 <joonas.jauhiainen@eficode.com>
2015-08-19 06:24:20 +0300

    Markers on the map works now.

--- assets/js/controllers/HomeController.js
+++ assets/js/controllers/HomeController.js
@@ -4,15 +4,49 @@ app.controller('HomeController', ['$scope', 'firehydrants', function($scope, fir
 	$scope.fireHydrantContainerClass="hidden";
 	$scope.isServerError = false;
 	$scope.serverErrorMessage = "";
+	$scope.firehydrants = [];
+	$scope.markers = [];
 	$scope.$watch("showFireHydrantContainer", function(){
 		if ($scope.showFireHydrantContainer === true){
-			fireHydrantService.getFireHydrants().success(function(data){
-				$scope.firehydrants = data;
-				console.log($scope.firehydrants);
-			})
-			.error(function(err){
-				$scope.isServerError = true;
-			});
+			$scope.mapContainerClass = "col-md-8";
+			$scope.fireHydrantContainerClass = "col-md-4";
+			getFireHydrants();
+		}
+		else {
+			$scope.mapContainerClass = "col-md-12";
+			$scope.fireHydrantContainerClass = "hidden";
 		}
 	});
+
+	function getFireHydrants(){
+		fireHydrantService.getFireHydrants().success(function(data){
+			console.log(data.length);
+			if(data.length != $scope.firehydrants.length){
+				$scope.firehydrants = data;
+				angular.forEach($scope.firehydrants, function(val){
+					$scope.markers.push(createMapMarker(val));
+				});
+			}
+		})
+		.error(function(err){
+			if(err === null){
+				$scope.serverErrorMessage = "Taustajärjestelmässä häiriöitä, ole hyvä ja ota yhteyttä palvelun ylläpitoon.";
+			}
+			console.log(err);
+			$scope.isServerError = true;
+		});
+	}
+
+	function createMapMarker(data){
+		var marker = {
+			id: data.id,
+			latitude: data.latitude,
+			longitude: data.longitude
+		};
+		return marker;
+	}
+
+
+	getFireHydrants();
+
 }]);
