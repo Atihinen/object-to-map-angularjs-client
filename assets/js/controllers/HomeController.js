7cb845f112e6b1ac77d8bf2b48a5f20335b7cf7e
Joonas Jauhiainen2 <joonas.jauhiainen@eficode.com>
2015-08-19 16:27:54 +0300

    Getting marker data works now. Working with adding markers.

--- assets/js/controllers/HomeController.js
+++ assets/js/controllers/HomeController.js
@@ -1,11 +1,46 @@
 app.controller('HomeController', ['$scope', 'firehydrants', function($scope, fireHydrantService){
-	$scope.map = {center: {latitude: 60.218184, longitude: 24.812150}, zoom: 15};
+	$scope.map = {
+		center: {
+			latitude: 60.218184,
+			longitude: 24.812150
+		},
+		zoom: 15,
+		markers: [],
+		window: {
+            marker: {},
+            show: false,
+            closeClick: function() {
+                this.show = false;
+            },
+            options: {}, // define when map is ready
+            title: '',
+            trunkLineDiameter: '',
+            description: ''
+        },
+        markersEvents: {
+            click: function(marker, eventName, model, arguments) {
+                console.log('Marker was clicked (' + marker + ', ' + eventName);//+', '+mydump(model, 0)+', '+mydump(arguments)+')');
+				console.log(marker);
+				console.log(model);
+                $scope.map.window.model = model;
+                updateMapWindow(model);
+                $scope.map.window.show = true;
+            }
+        },
+        events: {
+        	click: function(m, e, model, arguments){
+        		console.log(m);
+        		console.log(e);
+        		console.log(model);
+        		console.log(arguments);
+        	}
+        }
+	};
 	$scope.mapContainerClass="col-md-12";
 	$scope.fireHydrantContainerClass="hidden";
 	$scope.isServerError = false;
 	$scope.serverErrorMessage = "";
 	$scope.firehydrants = [];
-	$scope.markers = [];
 	$scope.$watch("showFireHydrantContainer", function(){
 		if ($scope.showFireHydrantContainer === true){
 			$scope.mapContainerClass = "col-md-8";
@@ -18,13 +53,28 @@ app.controller('HomeController', ['$scope', 'firehydrants', function($scope, fir
 		}
 	});
 
+	$scope.findMarker = function(id){
+		console.log(id);
+		var marker = {}
+		angular.forEach($scope.map.markers, function(m){
+			if(m.id === id){
+				marker = m;
+				return;
+			}
+		});
+		$scope.map.center.latitude = marker.latitude;
+		$scope.map.center.longitude = marker.longitude;
+		updateMapWindow(marker);
+        $scope.map.window.show = true;
+}
+
 	function getFireHydrants(){
 		fireHydrantService.getFireHydrants().success(function(data){
 			console.log(data.length);
 			if(data.length != $scope.firehydrants.length){
 				$scope.firehydrants = data;
 				angular.forEach($scope.firehydrants, function(val){
-					$scope.markers.push(createMapMarker(val));
+					$scope.map.markers.push(createMapMarker(val));
 				});
 			}
 		})
@@ -38,14 +88,45 @@ app.controller('HomeController', ['$scope', 'firehydrants', function($scope, fir
 	}
 
 	function createMapMarker(data){
+		/*var marker = new google.maps.Marker({
+            animation: google.maps.Animation.DROP,
+            position: new google.maps.LatLng(data.latitude, data.longitude),
+            title: data.category.name,
+            id: data.id
+        });*/
 		var marker = {
 			id: data.id,
 			latitude: data.latitude,
-			longitude: data.longitude
+			longitude: data.longitude,
+			title: data.category.name,
+			trunkLineDiameter: data.trunk_line_diameter,
+			description: data.description
 		};
 		return marker;
 	}
 
+	function updateMapWindow(model){
+		$scope.map.window.title = model.title;
+        $scope.map.window.description = model.description;
+        $scope.map.window.trunkLineDiameter = model.trunkLineDiameter;
+	}
+
+	/*var test = new google.maps.Marker({
+            map: $scope.map,
+            animation: google.maps.Animation.DROP,
+            position: new google.maps.LatLng(24.7, 65.3),
+            title: "test"
+        });*/
+
+	$scope.onMarkerClicked = function (m) {
+            //this.windowOptions = !this.windowOptions;
+            console.log('Marker was clicked');
+            console.log(m);
+        };
+
+        $scope.closeClick = function () {
+            this.window = false;
+        };
 
 	getFireHydrants();
 
